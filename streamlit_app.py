7428b1e8af7aab2819c0c6e78753801c1aa80e19
lulu08-G <lucas_91@live.fr>
2025-03-19 10:39:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,18 @@ import requests
 import json
 
 item_id = 17584
-url = f"https://api.dofusdu.de/dofus3/v1/fr/items/equipments/{item_id}"
 
-response = requests.get(url)
+url = f"https://api.dofusdu.de/dofus3/v1/fr/items/resources/search"
+params = {
+    "query": "",
+    "filter[crafted_item.id]": item_id
+}
+
+response = requests.get(url, params=params)
 
 if response.status_code == 200:
     data = response.json()
     print(json.dumps(data, indent=4, ensure_ascii=False))
 else:
     print(f"Erreur : {response.status_code}")
-
+    print(response.text)
