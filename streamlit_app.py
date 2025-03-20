638d35c36d5ccc7a9c86d0d5335d698be3f06f8c
lulu08-G <lucas_91@live.fr>
2025-03-20 13:58:59 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -270,7 +270,7 @@ elif page == "Page test":
 
             with cols[0]:
                 if image_url:
-                    st.image(image_url, width=50, use_container_width=True)
+                    st.image(image_url, width=20, use_container_width=True)
                 else:
                     st.write("❓")  # Icône manquante
 
